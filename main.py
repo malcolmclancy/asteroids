816a07fb7f307ba8631ee7b98296f4f180f15cd3
malcolmclancy <malcolm.clancy@hotmail.com>
2025-01-30 22:26:21 +1000

    Add resources for Player rendering and movement

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x= (SCREEN_WIDTH / 2), y= (SCREEN_HEIGHT / 2))
+
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill(0)
+        for obj in updatable:
+             obj.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
